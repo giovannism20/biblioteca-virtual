459f0d14612965e57e0825a5e3da1f2f6b15eb6a
AZilepx <liza.13quercia@gmail.com>
2025-05-24 12:20:29 -0300

    Separa responsabilidade
    Separa arquivos por entidades

--- main.py
+++ main.py
@@ -2,4 +2,24 @@ import biblioteca
 import livro
 
 if __name__ == "__main__":
-    biblioteca.main()
+    while True:
+        biblioteca = Biblioteca()
+        biblioteca.menu()
+        opcao = input("Escolha uma opção: ")
+        limpar_tela()
+
+        if opcao == "1":
+            biblioteca.cadastrar_livro()
+        elif opcao == "2":
+            biblioteca.listar_livros()
+        elif opcao == "3":
+            biblioteca.alterar_status()
+        elif opcao == "4":
+            biblioteca.buscar_livros()
+        elif opcao == "5":
+            biblioteca.exportar_lista()
+        elif opcao == "6":
+            print("Saindo...  Até a próxima!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
