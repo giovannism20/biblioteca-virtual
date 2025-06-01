15f0a29f683783ea87929d53cc684afe5775d1ad
Ana Clara Ayres <ayresnauna@gmail.com>
2025-06-01 18:14:21 -0300

    Cria função listagem
    Criamos uma função de listagem, reaproveitável para livros e para bibliotecas.
    Criamos uma função de exibir lista de livros
    
    Co-Authored-By: Romário França <111925796+romariouba@users.noreply.github.com>
    Co-Authored-By: Giovanni Martins <7796661+giovannism20@users.noreply.github.com>
    Co-Authored-By: AZilepx <171746371+AZilepx@users.noreply.github.com>
    Co-Authored-By: Jhonatan da Silva <74478725+corleoneo@users.noreply.github.com>

--- util.py
+++ util.py
@@ -1,15 +1,26 @@
 import os
 
-def exibir_lista_bibliotecas(bibliotecas: dict) -> list:
-    if not bibliotecas:
-        print("Ainda não possuímos bibliotecas cadastradas no sistema.")
+def exibir_lista_nomeada(entidades: dict, mensagem_vazia: str) -> list:
+    if not entidades:
+        print(mensagem_vazia)
         return []
 
-    print("Selecione a biblioteca pelo número:")
-    lista = list(bibliotecas.keys())
+    lista = list(entidades.keys())
     for i, nome in enumerate(lista, start=1):
         print(f"{i} - {nome}")
     return lista
+
+def exibir_lista_livros(livros: dict) -> list:
+    return exibir_lista_nomeada(
+        livros,
+        "Ainda não há livros cadastrados no sistema."
+    )
+
+def exibir_lista_bibliotecas(bibliotecas: dict) -> list:
+    return exibir_lista_nomeada(
+        bibliotecas,
+        "Ainda não há bibliotecas cadastradas no sistema."
+    )
 
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
